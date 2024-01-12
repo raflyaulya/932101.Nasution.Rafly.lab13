ea145b90c2f6af28fcb71df724e42bdabdfe84c0
raflyaulya <auliarafli819@gmail.com>
2024-01-13 00:53:40 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
+using _932101.Nasution.Rafly.lab13.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddTransient<IQuizService, QuizService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
